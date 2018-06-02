fde967838e58c2fafd2b4e1dc626e0a654ff6d33
肖盛锋 <xiaoshengfeng@cloudist.cc>
2018-06-02 22:25:45 +0800

    [FIX] 使用 pandas 的 concat 连接每次迭代的结果

--- statistics.py
+++ statistics.py
@@ -1,7 +1,5 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals, division, print_function
-
 import os
 import sys
 from pprint import pprint
@@ -30,7 +28,9 @@ def get_file():
         sys.exit(1)
     excel_dir = sys.argv[1]
     files_in_dir = os.listdir(excel_dir)
-    return [f for f in files_in_dir if '.xls' in f and OUTPUT_FILE_NAME not in f]  # .xls / .xlsx
+    return [
+        os.path.join(excel_dir, f) for f in files_in_dir if '.xls' in f and OUTPUT_FILE_NAME not in f
+    ]  # .xls / .xlsx
 
 
 def part_1_1():
@@ -75,7 +75,7 @@ def part_1_4():
 def part_1_5():
     part1_5 = init_an_order_dict(year_list_pured, init_value="")
     for i in xrange(0, len(year_list)):
-        if ';' in col_shen_qing_ren[i] and '大学' in col_shen_qing_ren[i]:
+        if ';' in col_shen_qing_ren[i] and u'大学' in col_shen_qing_ren[i]:
             part1_5[year_list[i]] += "%s " % (col_xu_hao[i])
     return part1_5
 
@@ -91,8 +91,8 @@ def part1():
     for k in keys:
         line = [
             company_code, k, part1_1[k], part1_2[k], part1_3[k],
-            "%s(%s个)" % (part1_4[k].strip(), 0 if part1_4[k].strip() == u'' else len(part1_4[k].strip().split(' '))),
-            "%s(%s个)" % (part1_5[k].strip(), 0 if part1_5[k].strip() == u'' else len(part1_5[k].strip().split(' ')))
+            u"%s(%s个)" % (part1_4[k].strip(), 0 if part1_4[k].strip() == u'' else len(part1_4[k].strip().split(' '))),
+            u"%s(%s个)" % (part1_5[k].strip(), 0 if part1_5[k].strip() == u'' else len(part1_5[k].strip().split(' ')))
         ]
         output.append(line)
     return output
@@ -102,12 +102,12 @@ def part3():
     output_list = list()
     for i in xrange(0, col_gong_kai_hao.count()):
         col_d_value = col_gong_kai_hao[i]
-        if unicode(col_yin_zheng_zhuan_li[i]) != 'nan':
+        if unicode(col_yin_zheng_zhuan_li[i]) != u'nan':
             for r in col_yin_zheng_zhuan_li[i].split('; '):
                 output_list.append(
                     (company_code, i+1, r, col_d_value)
                 )
-        if unicode(col_bei_yin_zheng_zhuan_li[i]) != 'nan':
+        if unicode(col_bei_yin_zheng_zhuan_li[i]) != u'nan':
             for s in col_bei_yin_zheng_zhuan_li[i].split('; '):
                 output_list.append(
                     (company_code, i+1, col_d_value, s)
@@ -135,26 +135,8 @@ def part4():
     return output_list
 
 
-def write_part1():
-    data_frame_output1 = pd.DataFrame(part1())
-    data_frame_output1.to_excel(
-        writer, sheet_name='part1', index=False,
-        header=['公司', '年份', '专利总数', '被引次数', '被引且出现在D列', '申请人数大于2', '申请人数大于2且含大学']
-    )
-
-
-def write_part2():
+def part2():
     pass
-
-
-def write_part3():
-    data_frame_output3 = pd.DataFrame(part3())
-    data_frame_output3.to_excel(writer, sheet_name='part3', header=['公司', '序号', 'A', 'B'], index=False)
-
-
-def write_part4():
-    data_frame_output4 = pd.DataFrame(part4())
-    data_frame_output4.to_excel(writer, sheet_name='part4', header=['公司', '序号', 'A', 'B'], index=False)
 
 
 if __name__ == '__main__':
@@ -168,6 +150,10 @@ if __name__ == '__main__':
 
     files = get_file()
     writer = pd.ExcelWriter(OUTPUT_FILE_NAME, engine='xlsxwriter')
+    part1_to_write = []
+    part2_to_write = []
+    part3_to_write = []
+    part4_to_write = []
 
     for xls in files:
         base_name = os.path.basename(xls)
@@ -176,35 +162,62 @@ if __name__ == '__main__':
 
         df = pd.read_excel(xls)
 
-        col_shen_qing_ri = df['申请日']
-        col_shen_qing_ren = df['申请人']  # I列
-        col_xu_hao = df['序号']
-        col_gong_kai_hao = df['公开（公告）号']  # D列
-        col_yin_zheng_zhuan_li = df['引证专利']  # R列
-        col_bei_yin_zheng_zhuan_li = df['被引证专利']  # S列
-        col_bei_yin_zheng_ci_shu = df['被引证次数']
-        col_yin_zheng_shen_qing_ren = df['引证申请人']  # T
-        col_bei_yin_zheng_shen_qing_ren = df['被引证申请人']  # U
+        col_shen_qing_ri = df[u'申请日']
+        col_shen_qing_ren = df[u'申请人']  # I列
+        col_xu_hao = df[u'序号']
+        col_gong_kai_hao = df[u'公开（公告）号']  # D列
+        col_yin_zheng_zhuan_li = df[u'引证专利']  # R列
+        col_bei_yin_zheng_zhuan_li = df[u'被引证专利']  # S列
+        col_bei_yin_zheng_ci_shu = df[u'被引证次数']
+        col_yin_zheng_shen_qing_ren = df[u'引证申请人']  # T
+        col_bei_yin_zheng_shen_qing_ren = df[u'被引证申请人']  # U
 
         year_list = [y.year for y in col_shen_qing_ri]
         year_list_pured = sorted(set(year_list))
 
         if part_arg == 'part1':
-            write_part1()
+            try:
+                part1_to_write.append(pd.DataFrame(part1()))
+            except:
+                print "%s part1 error, pass..." % company_code
         elif part_arg == 'part2':
-            write_part2()
+            pass
         elif part_arg == 'part3':
-            write_part3()
+            try:
+                part3_to_write.append(pd.DataFrame(part3()))
+            except:
+                print "%s part3 error, pass..." % company_code
         elif part_arg == 'part4':
-            write_part4()
+            try:
+                part4_to_write.append(pd.DataFrame(part4()))
+            except:
+                print "%s part4 error, pass..." % company_code
         elif part_arg == 'all':
-            write_part1()
-            write_part2()
-            write_part3()
-            write_part4()
+            try:
+                part1_to_write.append(pd.DataFrame(part1()))
+            except:
+                print "%s part1 error, pass..." % company_code
+            try:
+                part3_to_write.append(pd.DataFrame(part3()))
+            except:
+                print "%s part3 error, pass..." % company_code
+            try:
+                part4_to_write.append(pd.DataFrame(part4()))
+            except:
+                print "%s part4 error, pass..." % company_code
         else:
             print(help_text)
             sys.exit(1)
+
+    result1 = pd.concat(part1_to_write)
+    result1.to_excel(
+        writer, sheet_name='part1', index=False,
+        header=[u'公司', u'年份', u'专利总数', u'被引次数', u'被引且出现在D列', u'申请人数大于2', u'申请人数大于2且含大学']
+    )
+    result3 = pd.concat(part3_to_write)
+    result3.to_excel(writer, sheet_name='part3', header=[u'公司', u'序号', u'A', u'B'], index=False)
+    result4 = pd.concat(part4_to_write)
+    result4.to_excel(writer, sheet_name='part4', header=[u'公司', u'序号', u'A', u'B'], index=False)
 
     writer.save()
     print("\nDone! 结果已输出到 %s 中\n" % OUTPUT_FILE_NAME)
